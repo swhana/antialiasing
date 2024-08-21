0e2911c8f14ef0f8db5bd7d152933bda7b100056
swhana <swhan0428@gmail.com>
2024-08-21 16:48:18 +0900

    Create Grid - 80x80 10px 그리드 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const Grid = () => {
+    const cells = Array.from({ length: 6400 }, (_, index) => (
+        <div key={index} className="cell" />
+    ));
+
+    return <div className="grid">{cells}</div>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid />
+        </div>
+    );
 }
 
 export default App;
